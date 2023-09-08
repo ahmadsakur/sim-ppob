a7e5d623671329c37a193e161a0e90dc0ab9472f
ahmadsakur <sakuretsuu@gmail.com>
2023-09-08 23:48:55 +0700

    Create Navbar

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8 py-8 min-h-screen">
+    <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8 min-h-screen">
       <Navbar />
       <main>{children}</main>
     </div>
